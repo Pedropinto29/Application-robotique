6e0f657b97a1fbf3d7a3e2f74fac11e3822da567
Pedropinto29 <pedrodacostapinto@gmail.com>
2023-03-07 17:22:32 +0100

    Interface application

---
+++ app/src/main/java/com/example/robot/SecondFragment.kt
@@ -0,0 +1,63 @@
+package com.example.robot
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SeekBar
+import androidx.navigation.fragment.findNavController
+import com.example.robot.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    var rotationEndPoint = 0
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+
+        binding.rotationBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekbar: SeekBar?, progress: Int, p2: Boolean) {
+                binding.rotationText.text = "Turn " + (progress - 90).toString() + " °"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                if (seekBar != null) {
+                    //gets the end value from the seekBar, the value to send to arduino
+                    rotationEndPoint = seekBar.progress
+                }
+            }
+        })
+
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
